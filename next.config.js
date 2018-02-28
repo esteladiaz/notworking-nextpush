103a26e3d4f4308a5578470e4f4e9e28b14ecc40
Estela <diazsanchez.estela@gmail.com>
2018-02-27 16:33:14 -0800

    gh-pages next js routing

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
+const webpack = require('webpack')
+
+const isProd = (process.env.NODE_ENV || 'production') === 'production'
+
+const assetPrefix = isProd ? '/esteladiaz.github.io' : ''
+
 module.exports = {
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' }
-    }
-  }
+  exportPathMap: () => ({
+    '/': { page: '/' },
+    '/page1': { page: '/page1' },
+  }),
+  assetPrefix: assetPrefix,
+  webpack: config => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
+      }),
+    )
+
+    return config
+  },
 }

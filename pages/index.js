2ad56af5b163d14299c10e1d9f1ca8cc5101b435
Estela <diazsanchez.estela@gmail.com>
2018-02-27 16:34:14 -0800

    test

--- pages/index.js
+++ pages/index.js
@@ -17,5 +17,11 @@ export default () => (
       <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
       <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"/>
     </ul>
+    <div>
+      <Link href="/page1" as={`${process.env.ASSET_PREFIX}/page1`}>
+        <a>Go to page</a>
+      </Link>
+      Root page
+    </div>
   </Layout>
 )
